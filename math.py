b4e065f9e6277a2c4b97e67f05ad293be5897a82
NandeshwarVerma <nandan24x7@gmail.com>
2020-08-04 00:08:39 -0400

    Add files via upload

---
+++ math.py
@@ -0,0 +1,14 @@
+#MATH functions
+import math				#always import math 
+#infinity 
+
+x=float('inf')
+print(x)
+
+y=float('-inf')
+print(y)
+
+#ISINF function - to check whether it is infinity or not
+
+print(math.isinf(x))
+print(math.isinf(y))
